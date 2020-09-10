0b5257770e21be4d61d8a4a79a6353d822b08e67
Ayes <52951851+ayes-web@users.noreply.github.com>
2020-09-10 21:12:31 +0300

    Create kongregate-game-grabber.user.js

---
+++ kongregate-game-grabber.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         kongregate game grabber
+// @namespace    kongregate
+// @version      0.1
+// @description  finds the direct links to web games on kongregate
+// @author       ayes-web
+// @include      https://www.kongregate.com/games/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var regex = /game(\d+)\.konggames\.com\/gamez\/[0-9]{4}\/[0-9]{4}\/live\/index\.html/
+    var match = regex.exec(document.getElementById("game").innerHTML);
+
+    var info = document.createElement("div");
+    info.innerHTML = "<a href='https://" + match[0] + "' target='_blank' style='color:white; text-align: center; margin: 10px; font-size: 30px;'>Open in new page</a>";
+    var game_table = document.getElementById("floating_game_holder");
+    game_table.appendChild(info);
+})();
